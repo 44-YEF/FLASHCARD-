01fd73c5cbf5d56d554ce00d6e4cbaaa7958fce8
44-YEF <00001094862320SP@al.educacao.sp.gov.br>
2024-11-08 19:27:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um fluxograma?',
+    'O fluxograma é uma representação pictórica de um programa que auxilia na compreensão do fluxo de controle e dados no algoritmo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a função do CSS no HTML?',
+    'indica o aspecto que ele deve ter: a fonte, o fundo, o texto e etc'
+)
+
+criaCartao(
+    'Lingua Estrangeira',
+    'Como se diz bom dia em francês?',
+    'Oi em francês é Bonjour (bõjúr)'
+)
